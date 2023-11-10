ef8b08648abb73c58ae1bf57c2119b47c367d2df
Cosmin <kosmyn1994@gmail.com>
2023-11-10 16:31:54 +0100

    general commit

--- home.js
+++ home.js
@@ -16,29 +16,37 @@ const fetchData = () => {
     })
     .then((products) => {
       products.forEach((name) => {
+        const divGenerico = document.createElement("div");
+        divGenerico.className = "card mb-4 shadow-sm";
+        const divCol = document.createElement("div");
+        divCol.className = "col-md-4";
+
         const img = document.createElement("img");
         img.className = "card-img-top";
-        img.src = products.imageUrl;
+        img.src = name.imageUrl;
+        img.style = "object-fit: cover; height: 200px;";
         const divBody = document.createElement("div");
         divBody.className = "card-body";
         const h5 = document.createElement("h5");
         h5.className = "card-title";
-        h5.innerText = products.name;
+        h5.innerText = name.name;
         const p = document.createElement("p");
         p.className = "card-text";
-        p.innerText = products.descriprion;
-        const button = document.createElement("button");
+        p.innerText = name.description;
+        const button = document.createElement("a");
         button.className = "btn btn-sm btn-outline-secondary";
-        button.type = "button";
+        button.href = "./detail.html?productId=" + name._id;
         button.innerText = "Details";
         // APPEND
         divBody.appendChild(h5);
         divBody.appendChild(p);
         divBody.appendChild(button);
-        cardDiv.appendChild(img);
-        cardDiv.appendChild(divBody);
-
-        button.addEventListener;
+        divGenerico.appendChild(img);
+        divGenerico.appendChild(divBody);
+        divCol.appendChild(divGenerico);
+        cardDiv.appendChild(divCol);
       });
     });
 };
+
+fetchData();
